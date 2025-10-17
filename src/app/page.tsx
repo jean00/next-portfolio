d0de86ee90dd624d44e511b7371a465099239367
jean <jean.mosqueraescobar.external@snam.it>
2025-10-17 17:35:20 +0200

    init custom badge variant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,13 @@ const mainTech = ["React", "TypeScript", "Next.js", "Tailwind CSS"];
 export default function Home() {
   return (
     <div className="flex flex-col h-[calc(100vh-3rem)] justify-center items-center text-center">
-      <h1 className="text-6xl font-bold">
-        Hello, I'm{" "}
-        <span className="bg-gradient-to-b from-[#000000] to-[#00DDFF] bg-clip-text text-transparent">
-          Jean
-        </span>
+      <h1 className="text-6xl font-bold text-[color:--text]">
+        Hi, I'm <span className="bg-gradient-to-b from-[#000000] to-[#00DDFF] bg-clip-text text-transparent">Puli</span>
       </h1>
-      <p className="mt-4 text-5xl">Frontend developer</p>
-      <div className="flex gap-7">
+      <p className="text-5xl">Frontend developer</p>
+      <div className="flex gap-6">
         {mainTech.map((tech) => (
-          <Badge key={tech} className="mx-1 mt-4" variant={"outline"}>
+          <Badge key={tech} className="mx-1 mt-4" variant="tech">
             {tech}
           </Badge>
         ))}
