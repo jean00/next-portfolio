0d536e31564c819a83123e0dc5597e585cbf6e75
jean <jean.mosqueraescobar.external@snam.it>
2025-10-27 18:00:47 +0100

    Refactor About and Projects components for improved readability and functionality
    - Simplified imports in About and Projects components.
    - Consolidated JSX elements for better formatting in About component.
    - Enhanced Projects component to include GitHub and Live Demo links.
    - Updated project constants to include URLs for GitHub and Live Demo.
    - Adjusted image rendering in Projects component for better layout.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,31 +1,16 @@
 "use client";
 
-import {
-  CssIcon,
-  D3Icon,
-  HtmlIcon,
-  JavascriptIcon,
-  ReactIcon,
-  ReduxIcon,
-  TypescriptIcon,
-} from "@/constants/image";
+import { CssIcon, HtmlIcon, JavascriptIcon, ReactIcon, ReduxIcon, TypescriptIcon } from "@/constants/image";
 import Avatar from "@/assets/avatar.png";
 import SectionWrapper from "@/wrapper/section-wrapper";
-import { motion } from "motion/react";
 import Image from "next/image";
 
 const techs = [
   <ReactIcon key="react" className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />,
-  <TypescriptIcon
-    key="ts"
-    className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12"
-  />,
+  <TypescriptIcon key="ts" className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />,
   <HtmlIcon key="html" className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />,
   <CssIcon key="css" className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />,
-  <JavascriptIcon
-    key="js"
-    className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12"
-  />,
+  <JavascriptIcon key="js" className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />,
   <ReduxIcon key="redux" className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />,
 ];
 
@@ -33,14 +18,10 @@ const About = () => {
   return (
     <>
       <h2 className="text-4xl font-bold mb-4">
-        About{" "}
-        <span className="bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
-          Me
-        </span>
+        About <span className="bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">Me</span>
       </h2>
       <h2 className="text-lg lg:text-2xl mb-2 lg:mb-12">
-        I build front-ends with <strong>performance</strong> and{" "}
-        <strong>accessibility</strong> in mind.
+        I build front-ends with <strong>performance</strong> and <strong>accessibility</strong> in mind.
         <br />
       </h2>
       <div className="flex flex-col lg:flex-row gap-8 md:gap-16 h-[400px]">
@@ -53,19 +34,15 @@ const About = () => {
 
         <div className="flex flex-col lg:justify-between h-full text-center lg:text-left">
           <p className="text-xl text-gray-400 leading-relaxed mb-6">
-            Front-End Developer with 3+ years of experience using React and
-            TypeScript. Passionate about clean, maintainable UI and micro
-            front-end architectures. Based in Milan, Italy 🇮🇹.
+            Front-End Developer with 3+ years of experience using React and TypeScript. Passionate about clean, maintainable UI
+            and micro front-end architectures. Based in Milan, Italy 🇮🇹.
           </p>
           <div>
             <h2 className="text-lg font-bold mb-4">My tech stack</h2>
             <div className="flex flex-wrap justify-center md:justify-start gap-10 mb-4">
-              {techs.map((icon) => icon)}
+              {techs.map((icon) => icon)} <p className="text-xs text-center w-full">... and more on my resume</p>
             </div>
           </div>
-          <p className="text-xs text-center w-full">
-            ... and more on my resume
-          </p>
         </div>
       </div>
     </>
--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -1,25 +1,42 @@
 import { Discord, Todolist, WeatherForecast } from "@/constants/image";
+import { ElementType, JSX } from "react";
 
-export const projects = [
+interface Projects {
+  key: string;
+  image: ElementType;
+  title: string;
+  description: string;
+  background: string[];
+  githubUrl: string;
+  liveUrl: string;
+}
+
+export const projects: Projects[] = [
   {
     key: "discord-music-bot",
-    image: <Discord />,
+    image: Discord,
     title: "Discord music bot",
     description: "A simple music bot for Discord built by using Node Js.",
     background: ["#6B5BFF", "#563ACC"],
+    githubUrl: "https://github.com/jean00/discordbot",
+    liveUrl: "https://discord.com/oauth2/authorize?client_id=904003512927653899&scope=bot&permissions=3147776",
   },
   {
     key: "todo-list",
-    image: <Todolist />,
+    image: Todolist,
     title: "Todo List App",
     description: "A simple todo list app built by using React.",
     background: ["#007C91", "#00B8D4"],
+    githubUrl: "https://github.com/jean00/todolist",
+    liveUrl: "https://jean00.github.io/todolist/",
   },
   {
     key: "weather-forecast-app",
-    image: <WeatherForecast />,
+    image: WeatherForecast,
     title: "Weather Forecast App",
     description: "A simple weather forecast app built by using React.",
     background: ["#FF8A65", "#FF7043"],
+    githubUrl: "https://github.com/jean00/nextweatherapp",
+    liveUrl: "https://weatherapp-jean00.vercel.app/",
   },
 ];
